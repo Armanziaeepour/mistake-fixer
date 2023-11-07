85c1c919d44b90ce394efe8b913f3fde31de7934
Arman zp <armanzp@Armans-MacBook-Pro.local>
2023-11-07 20:17:29 +0330

    salam full code

---
+++ Salam.js
@@ -0,0 +1,17 @@
+let firstint = document.getElementById("inp")
+
+function wrote(){
+    let R = firstint.value.split(``);
+for (let i=0 ; i<R.length ; i++) {
+    if (R[i] == "=" ){
+        delete R[i] ;
+        delete R[i-1];
+    }
+}
+let result = R.join('') ;
+alert(result) ;
+}
+
+function remove () {
+firstint.value = '';
+}
